257426e1637adb65a657c36c903407816974d803
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-03-18 13:58:03 +0100

    Implement admin management page with user data fetching and display

--- src/app/(admin)/admin/admins/page.tsx
+++ src/app/(admin)/admin/admins/page.tsx
@@ -1,11 +1,27 @@
 "use client";
+import React, { useEffect, useState } from "react";
+import { UserTable } from "@/components/admincomps/user/datatable/UserTable";
+import { adminscolumns } from "@/components/admincomps/user/datatable/columns";
+import { User, users } from "@/components/admincomps/user/datatable/colsdata";
 
-import React, { useState, useEffect } from "react";
 import Admins from "./tabs/Admins";
 
+export async function getData(): Promise<User[]> {
+  return users;
+}
+
 function AdminManagementPage() {
+  const [data, setData] = useState<User[]>([]);
+
   useEffect(() => {
-    // Any code that accesses the document object should be placed here
+    async function fetchData() {
+      const result = await getData();
+      const filteredData = result.filter(
+        (user) => user.role === "admin" || user.role === "super admin"
+      );
+      setData(filteredData);
+    }
+    fetchData();
   }, []);
   return (
     <div className="rounded-3xl p-6">
@@ -25,8 +41,12 @@ function AdminManagementPage() {
           </button>
         </div>
       </div>
-
-      <Admins />
+      <div className="rounded-3xl px-8 py-6 flex flex-col gap-4 border border-neutral-200 bg-white">
+        <h2 className="text-xl font-semibold text-left">Administrators</h2>
+        <div>
+          <UserTable columns={adminscolumns} data={data} />
+        </div>
+      </div>
     </div>
   );
 }

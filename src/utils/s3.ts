3455c8608e4ee735e947a45f56d68602c976587b
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-04-03 23:25:29 +0100

    Enhance S3 utility by implementing a function to generate a signed upload URL with a unique filename

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,4 +1,6 @@
 import aws from "aws-sdk";
+import crypto, { randomBytes } from "crypto";
+import { promisify } from "util";
 
 const region = process.env.AWS_REGION;
 const bucketName = process.env.AWS_BUCKET_NAME;
@@ -12,7 +14,16 @@ const s3 = new aws.S3({
   signatureVersion: "4",
 });
 
+export async function generateUploadURL() {
+  const rawBytes = await randomBytes(16);
+  const fileName = rawBytes.toString("hex");
 
-export function generateUploadURL(){
-    const fileName = "random.jpg"
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Expires: 180,
+  };
+
+  const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
+  return uploadUrl;
 }

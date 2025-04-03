3d0ae5be60753d7f4611dc690f2ca130bfb11861
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-04-03 15:57:03 +0100

    Configure AWS S3 client with environment variables and add function to generate upload URL

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,18 @@
 import aws from "aws-sdk";
 
-const s3 = new aws.S3({})
+const region = process.env.AWS_REGION;
+const bucketName = process.env.AWS_BUCKET_NAME;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new aws.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: "4",
+});
 
 
+export function generateUploadURL(){
+    const fileName = "random.jpg"
+}

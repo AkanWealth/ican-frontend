cb8f4c2f75fc98d8866e542c579f9b1ce3df088d
Woods Francis <woodsedema@gmail.com>
2025-04-07 15:16:57 +0100

    Update page.tsx

--- src/app/(adminauth)/admin-login/page.tsx
+++ src/app/(adminauth)/admin-login/page.tsx
@@ -125,11 +125,11 @@ function AdminLogin() {
         localStorage.setItem("access_token", access_token);
         document.cookie = `access_token=${access_token}; path=/; secure; samesite=strict`;
 
-        if (user.role === "SUPER_ADMIN" || user.role === "ADMIN") {
-          router.push("/admin");
-        } else {
+        if (user.role === "MEMBER" ) {
           // Handle unauthorized access attempt
           router.push("/login");
+        } else {
+          router.push("/admin");
         }
         return <Toast type="success" message="Login successful" />;
       } catch (error) {
